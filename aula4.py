ca9c5cdb4e7fd0af7ba1625b19a253218486d2b6
Alesssandra F R <102836095+Alle84fr@users.noreply.github.com>
2025-04-06 19:10:16 -0300

    Create aula4.py
    primeiras aulas guanabara - 1°  mundo
    print texto
    imput de nome e ano 
    variável idade
    print
    obs - usando forma mais atualizada
    
    / https://www.cursoemvideo.com/curso/python-3-mundo-1/aulas/tratando-dados-e-fazendo-contas/modulos/tipos-primitivos-e-saida-de-dados/

---
+++ aula4.py
@@ -0,0 +1,18 @@
+print("Saluton Mondo")
+
+print(f"O resultado de 4**2 + (14/2) é {4**2 + (14/2)}")
+
+print("""\nPara criar variáveis - local de memória (volátil)
+                     - objeto é um pouco mais que uma variável
+                     - = revebe algo
+                     - '12' é uma string
+                     - 12 é um int""")
+
+nome = input("Digite o nome : ")
+ano = int(input("ano de nascimento: "))
+idade = 2025 - ano
+print(f"\n {nome}, você nasceu em {ano} e tem {idade}")
+print("""\nnome = input("Digite o nome : ")
+        ano = int(input("ano de nascimento: "))
+        idade = 2025 - ano
+        print(f" {nome}, você nasceu em {ano} e tem {idade}")""")
