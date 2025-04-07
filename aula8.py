ba450dc0e55ac7e5c53446b2fe1463a42abe6fe1
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-04-06 23:32:09 -0300

    add aula8.py cabeçalho comentado

---
+++ aula8.py
@@ -0,0 +1,7 @@
+
+# **************************************************************************************************#
+#                    AGORA COMEÇA PARTES QUE CONFUNDO OU NÃO LEMBRO OU ERRO                         #
+# **************************************************************************************************#
+
+# Modulos | Quebrando n° | cateto e Hipotenusa | Seno,cosseno e tangente | sorteando intem na lista 
+# tocando mp3 | manipulando texto | exercícios
