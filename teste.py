7da38fc61ddcb61b55bea8c2ca2212096d92389c
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-02 14:07:02 -0300

    testes foi feito direto no site, apenas coloquei resposta errada

---
+++ teste.py
@@ -0,0 +1,52 @@
+# conferindo conta de cabeça
+
+print(19//2)
+print(19%2)
+print(3*5+4**2)
+
+# não porei as peguntas corretas apenas as que errei 
+
+# Das opções abaixo, apenas uma NÃO É uma característica da linguagem Python. Marque a opção inválida da lista a seguir:
+# Nativamente compilada
+
+# Das opções abaixo, apenas que é característica da linguagem Python. Marque a opção inválida da lista a seguir:
+# orientada a obj
+# linguahem livre
+# multiplataformas
+
+#_________________________________________________________________________________________________________________
+
+# POO - # orientada a obj
+
+# pode criar classes e objetos no py
+
+# linguahem livre
+
+# software de código aberto, livre para ser usado, ditribuído e modificado
+
+# multiplataformas
+
+# executado em diversos sistemas operacionais, sem precisar reescrever o cód
+
+# Nativamente compilada (X)
+
+# é I N T E R P R E T A D A , o bytecode python é executado por uma máquina virtual python
+# a copilação não é diretamente para cód de máquina
+
+#___________________________ compilador x interpretador - resumo _____________________________
+
+# compilador
+
+# 1° lê todo o cód ANTES DE EXECUTAR
+# "Tradutor resume o livro todo para outra lingua e o entrega já pronto, ou para e devolve tudo mostrando onde há erros"
+# cód é traduzido uma vez para linguagem de máquina
+
+# interpretador
+
+# "tradutor traduz o livro conforme o leitor vai falando 'tradução ao vivo'"
+# cód é traduzido para bytecode - linguagem entre alta e baixa - e este bytecode é executado LINHA POR LINHA que será traduzido para 
+# linguagem de computador pela Máquina Virtual, em tempo real
+
+# o python é  híbrido
+
+# ele não compila direto porque usa a Máquina Virtual para interpretar o bytecode, porém não é interpertador puro porque existe o bytecode (pré tradução entre linguagem alta e baixa)
